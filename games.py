f0f0d58159aae4c0eaa7aeaada2dfd37aa29f87e
Giovanni <dallavecchiacareer@outlook.com>
2023-07-25 03:56:38 +0100

    Replace If/else by match/case

--- games.py
+++ games.py
@@ -1,3 +1,5 @@
+import platform
+
 # MAIN SCREEN TO SELECT WHICH GAME TO PLAY
 print("Welcome to my selections of games.")
 print("Enter the number of the game as shows below: ")
@@ -5,16 +7,34 @@ print("(1) Guess Number\nGame where ou have 10 tries to guess the number that th
 print("(2) Greater/Lower\nGame where you have clues if the entered number is greater or lower as the computer choses.")
 print("(3) The famous Rock/Paper/Scissors game. You going to play against the computer.")
 
+# SET THE PATH ACCORDING THE OS - Windows, Linux or macOS
+ 
+which_os = platform.system()
+set_path = ""
+
+match which_os:
+    case "Windows":
+        set_path = "C:\Users\dalla\Documents\python_projects"
+    case "Linux":
+        set_path = "/home/gio/Documents/python_projects"
+    case "Darwin":
+        # macBook Pro
+        set_path = "/Users/pro/Dcouments/python_projects"
+        # macBook Air
+    case _:
+        print("Cannot set the path, OS not identified!")
+
 gameChosed = input("\nWhich game would you like to play? ")
 
-if gameChosed == '1':
-    print("You want to play Guess Number")
-    exec(open("/home/gio/Documents/projects/python/guessNumberv1.py").read())
-elif gameChosed == '2':
-    print("You want to play Lower/Greater")
-    exec(open("/home/gio/Documents/projects/python/greaterLowerv1.py").read())
-elif gameChosed == '3':
-    print("You want to play Rock/Paper/Scissors")
-    exec(open("/home/gio/Documents/projects/python/rps.py").read())
-else:
-    print("Can see you are not in a mood today! Bye Bye")
+match gameChosed:
+    case 1:
+        print("You want to play Guess Number")
+        exec(open("/home/gio/Documents/projects/python/guessNumberv1.py").read())
+    case 2:
+        print("You want to play Lower/Greater")
+        exec(open("/home/gio/Documents/projects/python/greaterLowerv1.py").read())
+    case 3:
+        print("You want to play Rock/Paper/Scissors")
+        exec(open("/home/gio/Documents/projects/python/rps.py").read())
+    case _:
+        print("Can see you are not in a mood today! Bye Bye")
